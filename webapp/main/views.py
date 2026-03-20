1b6bd37c8ce1e5fe7964b847c93ad531d509172a
prplrose <158543118+prplrose@users.noreply.github.com>
2026-03-20 18:49:37 +0100

    Fixed vscode by refactoring back to original structure

---
+++ webapp/main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from qrgenerator.qrtoimg import get_base64
+
+# Create your views here.
+def home(request):
+    return render(request, "base.html")
+
+def generator(request):
+
+    text = request.GET.get('text', 'https://youtu.be/dQw4w9WgXcQ')
+    context = {}
+    try:
+        context['img'] = get_base64(text)
+
+    except Exception as e:
+        context['error'] = e
+
+    return render(request, "generator.html", context)
+
+def yourqr(request):
+    return render(request, "yourqr.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
