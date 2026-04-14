3afb62be978d7eb5c734fc36ae7ab91f4ac8a0f7
whatever123882 <szymonkonz@gmail.com>
2026-04-14 14:30:04 +0200

    startup

---
+++ webapp/main/management/commands/startup.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        call_command("migrate")
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin1",
+                email="admin@admin.com",
+                password="admin1"
+            )
+            print("Admin created")
+        else:
+            print("Admin already exists")
