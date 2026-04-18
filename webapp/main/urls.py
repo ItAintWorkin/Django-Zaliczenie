e4a39fca7ef87896897ca67db79b698416d3282c
whatever123882 <szymonkonz@gmail.com>
2026-04-18 10:56:18 +0200

    Update urls debug path deleted.py

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path("", views.home, name = "home"),
     path("generator", views.generator, name = "generator"),
     path("yourqr", views.yourqr, name = "yourqr"),
-    path("debug", views.debug, name = "debug"),
     path("login", views.login_view, name = "login"),
     path("register", views.register, name = "register"),
     path("logout", views.logout_view, name="logout"),
